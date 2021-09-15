a0eb98a33e8f72e259f05110d9d46669b42ba31c
Blaine Anderson <BlaineAndersonDev@Gmail.com>
2021-09-15 11:09:22 -0700

    Server and Client communicate properly

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,15 @@
+// We import React here in order to use it.
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// This allows us to render and alter content dynamically on the DOM (https://reactjs.org/docs/react-dom.html)
+import { render } from 'react-dom';
+// This allows us to create a front-end 'Router', to allow the user to navigate throughout our components without having to reload the page each time (https://reacttraining.com/react-router/web/api/BrowserRouter)
+import { BrowserRouter } from 'react-router-dom';
+// This imports our actual App in order to display it to our browser.
+import App from './App.js';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// 'render' is what allows the App to be displayed. It's held inside of a 'Router' (BrowserRouter) so in the future we can implement additional route options. The entire thing is rendered at the element 'id' via HTML.
+render((
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>
+), document.getElementById('root'));
